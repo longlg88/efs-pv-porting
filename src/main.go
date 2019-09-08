32fc522507b4f56fbd1666dfc92e281b42d87f39
longlg88 <longlg88@naver.com>
2019-09-08 18:18:38 +0900

    Test

--- src/main.go
+++ src/main.go
@@ -47,6 +47,8 @@ func main() {
 			panic(err.Error())
 		}
 		fmt.Printf("There are %d PVs in the cluster\n", len(pvs.Items))
+		fmt.Printf("The list is ---\n")
+		fmt.Printf("|" + pvs.Items + "|")
 
 		/*
 			pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
